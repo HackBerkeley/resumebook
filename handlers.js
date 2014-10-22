6e549485ba4dfc40b0037ce2c15e5607bd91de70
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-10-21 18:08:56 -0700

    Update package.json

--- handlers.js
+++ handlers.js
@@ -7,7 +7,7 @@ function home(response, postData) {
 }
 
 function upload(response, postData) {
-    
+
     var file                 = JSON.parse(postData),
         fileRootName         = file.name.split('.').shift(),
         fileExtension        = file.name.split('.').pop(),
@@ -16,24 +16,24 @@ function upload(response, postData) {
         filePath             = fileRootNameWithBase + '.' + fileExtension,
         fileID               = 2,
         fileBuffer;
-    
+
     while (fs.existsSync(filePath)) {
         filePath = fileRootNameWithBase + '(' + fileID + ').' + fileExtension;
         fileID += 1;
     }
-    
+
     file.contents = file.contents.split(',').pop();
-    
+
     fileBuffer = new Buffer(file.contents, "base64");
-    
+
     if (config.s3_enabled) {
 
         var knox = require('knox'),
             client = knox.createClient(config.s3),
             headers = {'Content-Type': file.type};
-        
+
         client.putBuffer(fileBuffer, fileRootName, headers, function (err, res) {
-            
+
             if (typeof res !== "undefined" && 200 === res.statusCode) {
                 console.log('Uploaded to: %s', res.client._httpMessage.url);
                 response.statusCode = 200;
@@ -41,10 +41,10 @@ function upload(response, postData) {
                 console.log('Upload failed!');
                 response.statusCode = 500;
             }
-            
+
             response.end();
         });
-        
+
     } else {
         fs.writeFileSync(filePath, fileBuffer);
         response.statusCode = 200;
@@ -63,7 +63,7 @@ function serveStatic(response, pathname, postData) {
             'js'  : 'application/javascript',
             'png' : 'image/png'
         };
-    
+
     response.writeHead(200, {'Content-Type': extensionTypes[extension]});
     response.end(fs.readFileSync('./static' + pathname));
 }
